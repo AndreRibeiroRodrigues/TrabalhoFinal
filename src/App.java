989eedb65a73f2391b36a0e746f9b6a57ebe26cd
AndreRRodrigues <83727384+AndreRibeiroRodrigues@users.noreply.github.com>
2023-06-13 19:30:08 -0300

    save

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int escolha = 0;
+        do{
+
+        }while(escolha == 5);
+
     }
+    static void login(){
+
+    }
+    
 }
